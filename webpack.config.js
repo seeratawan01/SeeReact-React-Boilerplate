502019293788e7c401fa2cce64056be7b21aafd1
Xuntron <seeratsdsking@gmail.com>
2020-03-22 16:57:44 +0500

    eslint add and configured

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,12 @@ module.exports = {
     module: {
         rules: [
             {
+                enforce: 'pre',
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'eslint-loader',
+            },
+            {
                 test: /\.(js)$/,
                 use: 'babel-loader'
             },
